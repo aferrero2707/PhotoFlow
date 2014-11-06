1d7c20e8ece8a5a6f042a886ffc2e73cd5502c2b
Ferrero Andrea <aferrero1975@gmail.com>
2014-11-06 14:32:10 +0100

    Fixed mistake in build.sh for debug case

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,7 @@ rm CMakeCache.txt
 
 if [ x"$1" = "xdebug" ]; then
     #export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:PATH_TO_VIPS_INSTALL_WITH_DEBUG/lib/pkgconfig"
-    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$(pwd)/Debug  -DINSTALL_PREFIX=$(pwd)/Debug../
+    cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$(pwd)/Debug  -DINSTALL_PREFIX=$(pwd)/Debug ../
     make VERBOSE=1 install
 else
     #export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:PATH_TO_VIPS_INSTALL/lib/pkgconfig"
