81aea0c5cdc62a89c926293ea77e0bcff7efd518
Ferrero Andrea <aferrero1975@gmail.com>
2014-11-16 20:09:31 +0100

    Fixed crash when closing last image

--- src/base/photoflow.hh
+++ src/base/photoflow.hh
@@ -66,7 +66,7 @@ namespace PF
     static PhotoFlow& Instance();
 
     Image* get_active_image() { return active_image; }
-    void set_active_image(Image* i) { active_image = i; }
+    void set_active_image(Image* i) { active_image = i; std::cout<<"Active image: "<<i<<std::endl; }
 
     void set_new_op_func( new_op_func_t f ) { new_op_func = f; }
     void set_new_op_func_nogui( new_op_func_t f ) { new_op_func_nogui = f; }
