890fffa81ce71a6927b45fba3905876683ca11c0
aferrero2707 <aferrero1975@gmail.com>
2015-03-03 17:22:36 +0100

    OSX debugging

--- src/operations/curves.cc
+++ src/operations/curves.cc
@@ -84,7 +84,7 @@ void PF::CurvesPar::update_curve( PF::Property<PF::SplineCurve>& curve,
     }
     grey_curve.get().get_deltas( Greyvec );
   */
-  std::cout<<"CurvesPar::update_curve() called."<<std::endl;
+  std::cout<<"CurvesPar::update_curve() called."<<std::endl;std::cout.flush();
   for(int i = 0; i <= FormatInfo<unsigned char>::RANGE; i++) {
     float x = ((float)i)/FormatInfo<unsigned char>::RANGE;
     float y = curve.get().get_delta( x );
@@ -107,19 +107,19 @@ VipsImage* PF::CurvesPar::build(std::vector<VipsImage*>& in, int first,
   VipsImage* out = PF::OpParBase::build( in, first, imap, omap, level );
 
   if( grey_curve.is_modified() ) {
-	  std::cout<<"update_curve( grey_curve, Greyvec8, Greyvec16 );"<<std::endl;
+	  std::cout<<"update_curve( grey_curve, Greyvec8, Greyvec16 );"<<std::endl;std::cout.flush();
 	    update_curve( grey_curve, Greyvec8, Greyvec16 );
   }
 
   if( R_curve.is_modified() || G_curve.is_modified() || 
       B_curve.is_modified() || RGB_curve.is_modified() ) {
-	    std::cout<<"update_curve( R_curve, RGBvec8[0], RGBvec16[0] );"<<std::endl;
+	    std::cout<<"update_curve( R_curve, RGBvec8[0], RGBvec16[0] );"<<std::endl;std::cout.flush();
 	    update_curve( R_curve, RGBvec8[0], RGBvec16[0] );
-	    std::cout<<"update_curve( G_curve, RGBvec8[1], RGBvec16[1] );"<<std::endl;
+	    std::cout<<"update_curve( G_curve, RGBvec8[1], RGBvec16[1] );"<<std::endl;std::cout.flush();
 	    update_curve( G_curve, RGBvec8[1], RGBvec16[1] );
-	    std::cout<<"update_curve( B_curve, RGBvec8[2], RGBvec16[2] );"<<std::endl;
+	    std::cout<<"update_curve( B_curve, RGBvec8[2], RGBvec16[2] );"<<std::endl;std::cout.flush();
 	    update_curve( B_curve, RGBvec8[2], RGBvec16[2] );
-	    std::cout<<"update_curve( RGB_curve, RGBvec8[3], RGBvec16[3] );"<<std::endl;
+	    std::cout<<"update_curve( RGB_curve, RGBvec8[3], RGBvec16[3] );"<<std::endl;std::cout.flush();
 	    update_curve( RGB_curve, RGBvec8[3], RGBvec16[3] );
     for(int i = 0; i <= FormatInfo<unsigned char>::RANGE; i++) {
       for(int j = 0; j < 3; j++) RGBvec8[j][i] += RGBvec8[3][i];
