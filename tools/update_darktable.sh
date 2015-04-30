df81ef7ae4e824ad5bfce59a2704c37db67a9eb2
aferrero2707 <aferrero1975@gmail.com>
2015-04-30 14:41:32 +0200

    Updated list of darktable source files to be automatically copied

--- tools/update_darktable.sh
+++ tools/update_darktable.sh
@@ -3,7 +3,7 @@
 
 dtdir="$1"
 cp "$dtdir/src/external/adobe_coeff.c" "$dtdir/src/external/wb_presets.c" src/dt/external
-cp "$dtdir/src/common/colormatrices.c" \
-  "$dtdir/src/common/colorspaces.h" \
-  "$dtdir/src/common/colorspaces.c" \
-  "$dtdir/src/common/srgb_tone_curve_values.h" src/dt/common
+cp "$dtdir/src/common/colormatrices.c" src/dt/common
+cp "$dtdir/src/common/srgb_tone_curve_values.h" src/dt/common
+#"$dtdir/src/common/colorspaces.h"
+#"$dtdir/src/common/colorspaces.c"
