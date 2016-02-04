9c2ab2c7f0a3843c6ce17e946d40e7b53e932861
aferrero2707 <aferrero1975@gmail.com>
2016-02-04 08:56:13 +0100

    Added verbose messages

--- src/base/imageprocessor.cc
+++ src/base/imageprocessor.cc
@@ -238,14 +238,14 @@ void PF::ImageProcessor::run()
         break;
       case IMAGE_SAMPLE:
         if( !request.image ) continue;
-        //std::cout<<"PF::ImageProcessor::run(): locking image..."<<std::endl;
+        std::cout<<"PF::ImageProcessor::run(): locking image..."<<std::endl;
         request.image->sample_lock();
-        //std::cout<<"PF::ImageProcessor::run(IMAGE_SAMPLE): image locked."<<std::endl;
+        std::cout<<"PF::ImageProcessor::run(IMAGE_SAMPLE): image locked."<<std::endl;
         if( (request.area.width!=0) && (request.area.height!=0) )
           request.image->do_sample( request.layer_id, request.area );
         request.image->sample_done_signal();
         request.image->sample_unlock();
-        //std::cout<<"PF::ImageProcessor::run(IMAGE_SAMPLE): sampling done."<<std::endl;
+        std::cout<<"PF::ImageProcessor::run(IMAGE_SAMPLE): sampling done."<<std::endl;
         break;
       case IMAGE_UPDATE:
         if( !request.pipeline ) continue;
