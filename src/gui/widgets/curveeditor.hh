90b7737a7ff6736e79d6d1283ca2f694c30cd493
aferrero2707 <aferrero1975@gmail.com>
2016-02-22 17:22:05 +0100

    [curves] added point adjustment with keyboard arrows

--- src/gui/widgets/curveeditor.hh
+++ src/gui/widgets/curveeditor.hh
@@ -41,6 +41,7 @@ namespace PF {
 
   class CurveArea: public Gtk::DrawingArea
   {
+    //Gtk::DrawingArea area;
     SplineCurve curve;
 
     int border_size;
@@ -86,6 +87,7 @@ namespace PF {
 
     int curve_area_width, curve_area_height;
     CurveArea* curve_area;
+    Gtk::EventBox curve_area_ebox;
 
     int grabbed_point;
 
@@ -94,6 +96,7 @@ namespace PF {
     bool inhibit_value_changed;
 
     bool handle_curve_events(GdkEvent* event);
+    bool on_key_press_or_release_event(GdkEventKey* event);
 
   public:
     CurveEditor(OperationConfigGUI* dialog, std::string pname, CurveArea* ca,
