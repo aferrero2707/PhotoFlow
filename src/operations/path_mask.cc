1fdc9b4d91f0d4868462518013968407107bac00
aferrero2707 <aferrero1975@gmail.com>
2016-06-09 12:02:17 +0200

    [tools/path mask] re-enabled output tile cache to avoid corrupted falloff rendering

--- src/operations/path_mask.cc
+++ src/operations/path_mask.cc
@@ -424,7 +424,7 @@ VipsImage* PF::PathMaskPar::build(std::vector<VipsImage*>& in, int first,
   }
 
   VipsImage* cached = out;
-  /*
+  /**/
   VipsAccess acc = VIPS_ACCESS_RANDOM;
   bool threaded = true, persistent = false;
   if( vips_tilecache(out, &cached,
@@ -434,7 +434,7 @@ VipsImage* PF::PathMaskPar::build(std::vector<VipsImage*>& in, int first,
     return NULL;
   }
   PF_UNREF( out, "GaussBlurPar::build(): iter_in unref" );
-  */
+  /**/
   return cached;
 }
 
