b4a255dc72435ea867ff15d829a77113eca96bf6
aferrero2707 <aferrero1975@gmail.com>
2016-06-09 10:55:53 +0200

    [base] divided by 2 the pixel differences for grain extract/merge blend modes

--- src/base/blend_grain_extract.hh
+++ src/base/blend_grain_extract.hh
@@ -50,7 +50,7 @@ public:
   {
     pos = x;
     for( ch=CHMIN; ch<=CHMAX; ch++, pos++ ) {
-      ptop = ( ((typename FormatInfo<T>::PROMOTED)bottom[pos])-((typename FormatInfo<T>::PROMOTED)top[pos])+FormatInfo<T>::HALF );
+      ptop = ( (((typename FormatInfo<T>::PROMOTED)bottom[pos])-((typename FormatInfo<T>::PROMOTED)top[pos]))/2 + FormatInfo<T>::HALF );
       clip( opacity*ptop + (1.0f-opacity)*bottom[pos], out[pos] );
       //std::cout<<"  out="<<(int)out[pos]<<std::endl;
     }
@@ -74,7 +74,7 @@ public:
 
     pos = x;
     for( ch=CHMIN; ch<=CHMAX; ch++, pos++ ) {
-      ptop = ( ((typename FormatInfo<T>::PROMOTED)bottom[pos])-((typename FormatInfo<T>::PROMOTED)top[pos])+FormatInfo<T>::HALF );
+      ptop = ( (((typename FormatInfo<T>::PROMOTED)bottom[pos])-((typename FormatInfo<T>::PROMOTED)top[pos]))/2+FormatInfo<T>::HALF );
       clip( opacity_real*ptop + (1.0f-opacity_real)*bottom[pos], out[pos] );
       //std::cout<<"  out="<<(int)out[pos]<<std::endl;
     }
