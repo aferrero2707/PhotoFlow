f942e3e60b55ea3c56d639fc64ca0f065a981fe4
Sven Claussner <scit.2010@users.sourceforge.net>
2016-09-05 15:48:50 +0200

    Make build.sh and build_all.sh controllable by environment variables
    Make build.sh and build_all.sh controllable by environment variables, so we
    can install to almost arbitrary directories and control the script execution
    in a continuous integration environment.

--- build/build_all.sh
+++ build/build_all.sh
@@ -1,45 +1,62 @@
 #! /bin/bash
+#  Script:    build_all.sh
+#  Purpose:   Update (optionally) and build libvips. Build PhotoFlow.
+#  Call:      build_all.sh <Build-Type>
+#             Build-Type: debug for a Debug Build,
+#                         test  for a Test Build,
+#                         else for a Release Build.
+#  Environment Variables:
+#  BUILD_TYPE     : the Build-Type as above. Optional.
+#  CLEAN_VIPS     : 1 if VIPS shall be built from scratch. Optional.
+#                   Disabled by default.
+#  INSTALL_PREFIX : the root directory for VIPS' and PhotoFlow's builds.
+#                   Optional. If omitted, then this is the current directory.
 #  UPDATE_VIPS    : 1 if VIPS shall be updated before build. Optional.
 #                   Enabled by default.
+#  BUILD_VIPS     : 1 if VIPS shall be built. Optional.
+#                   Enabled by default.
+#  Author, License:
+#    Copyright (C) 2014, 2016 Ferrero Andrea, Sven Claussner
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-vips_install=$(pwd)/VIPS/build
-rebuild_VIPS=1
-update_VIPS=1
-if [ ${rebuild_VIPS} -eq 1 ]; then
-		rm -rf ${vips_install}
-		mkdir -p ${vips_install}
-		cd VIPS
+# Evaluate arguments and environment variables.
+## BUILD_TYPE: use environment variable BUILD_TYPE, $1 or Release (in this order)
+BUILD_TYPE=${BUILD_TYPE:-${1:-Release}}
+## Capitalize build_type to tolerate misspellings.
+export BUILD_TYPE=\
+$(echo ${BUILD_TYPE} | tr '[:upper:]' '[:lower:]' | sed 's/.*/\u&/')
 
-		if [ ${update_VIPS} -eq 1 ]; then
-        if [ -e libvips ]; then
-            cd libvips
-            git pull
-        else
-				    git clone https://github.com/jcupitt/libvips.git
-				    cd libvips
-        fi
-    else
-				cd libvips
-		fi
+if [ "${BUILD_TYPE}" == "Debug" ]; then
+    vips_enable_debug="yes"
+else
+    vips_enable_debug="no"
+fi
 
-		FLAGS="-O2 -msse4.2 -ffast-math"
-		FLAGS="$FLAGS -ftree-vectorize"
-		CFLAGS="$FLAGS" CXXFLAGS="$FLAGS -fpermissive" \
-				./configure --prefix=${vips_install} --disable-gtk-doc --disable-gtk-doc-html \
-				--disable-introspection --enable-debug=no --without-python  --without-magick --without-libwebp\
-				--enable-pyvips8=no --enable-shared=no --enable-static=yes
-		if [ $? -ne 0 ]; then
-				echo "VIPS configure failed"
-				exit 1
-		fi
-    #make clean depclean
-		make install
-		if [ $? -ne 0 ]; then
-				echo "VIPS compilation failed"
-				exit 1
-		fi
+## INSTALL_PREFIX: use environment variable INSTALL_PREFIX or current directory
+## (in this order)
+export INSTALL_PREFIX=${INSTALL_PREFIX:-$(pwd)}
 
-		cd ../..
+## Install VIPS to a separate directory than PhotoFlow.
+export VIPS_INSTALL_PREFIX=${INSTALL_PREFIX}/VIPS/${BUILD_TYPE}
+
+## Report detected settings.
+echo Building VIPS...
+echo Install VIPS to: ${VIPS_INSTALL_PREFIX}
+echo Build type: ${BUILD_TYPE}
+echo Include debug information: ${vips_enable_debug}
+
 ## UPDATE_VIPS: use environment variable or default value 1.
 update_VIPS=${UPDATE_VIPS:-1}
 if [ ${update_VIPS} -eq 1 ]; then
@@ -48,9 +65,31 @@ else
     echo "Update VIPS: no"
 fi
 
-#rm -rf Release
-mkdir -p Release
-cd Release
+## CLEAN_VIPS: use environment variable or default value 0.
+clean_VIPS=${CLEAN_VIPS:-0}
+if [ ${clean_VIPS} -eq 1 ]; then
+    echo "Clean VIPS source directory: yes"
+else
+    echo "Clean VIPS source directory: no"
+fi
+
+## BUILD_VIPS: use environment variable or default value 1.
+build_VIPS=${BUILD_VIPS:-1}
+if [ ${build_VIPS} -eq 1 ]; then
+    echo "Build VIPS: yes"
+else
+    echo "Build VIPS: no"
+fi
+
+# Rebuild and conditionally update.
+if [ ${build_VIPS} -eq 1 ]; then
+    # store current directory
+    export OLD_PWD=$(pwd)
+
+    # clean up
+    rm -rf ${VIPS_INSTALL_PREFIX}
+    mkdir -p ${VIPS_INSTALL_PREFIX}
+
     # get update
     if [ ${update_VIPS} -eq 1 ]; then
         ../tools/update_libvips.sh
@@ -59,5 +98,40 @@ cd Release
 
     # run libvips bootstrapper
     ./autogen.sh
-export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:${vips_install}/lib/pkgconfig"
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$(pwd) -DINSTALL_PREFIX=$(pwd) ../../ && make install
+
+    export FLAGS="-O2 -msse4.2 -ffast-math"
+    export FLAGS="${FLAGS} -ftree-vectorize"
+    export CFLAGS="${FLAGS}"
+    export CXXFLAGS="${FLAGS} -fpermissive"
+    ./configure --prefix=${VIPS_INSTALL_PREFIX} \
+    --disable-gtk-doc --disable-gtk-doc-html \
+    --disable-introspection --enable-debug==${vips_enable_debug} \
+    --without-python --without-magick --without-libwebp \
+    --enable-pyvips8=no --enable-shared=yes --enable-static=no
+    if [ $? -ne 0 ]; then
+        echo "VIPS configure failed"
+        exit 1
+    fi
+
+    if [ ${clean_vips} -eq 1 ]; then
+        make clean depclean
+    fi
+
+    make
+    if [ $? -ne 0 ]; then
+        echo "VIPS compilation failed"
+        exit 1
+    fi
+
+    make install
+    if [ $? -ne 0 ]; then
+        echo "VIPS installation failed"
+        exit 1
+    fi
+
+    # change back to former directory
+    cd -
+fi
+
+# Exit this script and continue with the PhotoFlow build.
+exec ./build.sh
