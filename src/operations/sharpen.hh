c8e8b3587a37c18237a230cf1cebce8eb9d5187b
aferrero2707 <aferrero1975@gmail.com>
2016-09-11 01:09:00 +0200

    [tools/sharpen] added channel selector

--- src/operations/sharpen.hh
+++ src/operations/sharpen.hh
@@ -54,6 +54,7 @@ namespace PF
 
     bool has_intensity() { return false; }
     bool needs_caching();
+    bool has_target_channel() { return true; }
       
     VipsImage* build(std::vector<VipsImage*>& in, int first, 
 		     VipsImage* imap, VipsImage* omap, 
