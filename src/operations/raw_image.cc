b0e10410f9b7e3807bebcd129d1fcf11d2849881
aferrero2707 <aferrero1975@gmail.com>
2016-10-07 23:04:05 +0200

    [tools/raw processing] fixed bayer pattern decoding

--- src/operations/raw_image.cc
+++ src/operations/raw_image.cc
@@ -334,8 +334,8 @@ PF::RawImage::RawImage( const std::string _fname ):
     for(col=0; col<iwidth; col++) {
       int col2 = col + crop_x;
       int row2 = row + crop_y;
-      //unsigned char color = (is_xtrans()) ? r->cfa.getColorAt(col2,row2) : r->cfa.getColorAt(col,row);
-      unsigned char color = (is_xtrans()) ? r->cfa.getColorAt(col2,row2) : FC(col,row);
+      unsigned char color = (is_xtrans()) ? r->cfa.getColorAt(col2,row2) : r->cfa.getColorAt(col,row);
+      //unsigned char color = (is_xtrans()) ? r->cfa.getColorAt(col2,row2) : FC(col,row);
       float val = 0;
       float nval = 0;
       switch(r->getDataType()) {
