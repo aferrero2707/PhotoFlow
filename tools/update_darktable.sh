1ffa05caa86878532b6ab62d6c93685afdf0dcfc
aferrero2707 <aferrero1975@gmail.com>
2016-11-04 08:40:02 +0100

    Updated Darktable import script

--- tools/update_darktable.sh
+++ tools/update_darktable.sh
@@ -21,4 +21,13 @@ cp "$dtdir/src/common/srgb_tone_curve_values.h" src/dt/common
 #"$dtdir/src/common/colorspaces.h"
 #"$dtdir/src/common/colorspaces.c"
 
+# Patches
+sed -i .bak 's/TRUE/(boolean)TRUE/g' src/external/rawspeed/RawSpeed/DngDecoderSlices.cpp
+sed -i .bak 's/return true/return (boolean)TRUE/g' src/external/rawspeed/RawSpeed/X3fDecoder.cpp
+sed -i .bak 's/return false/return (boolean)FALSE/g' src/external/rawspeed/RawSpeed/X3fDecoder.cpp
+#rm -f src/external/rawspeed/RawSpeed/DngDecoderSlices.cpp.bak
+sed -i .bak 's%darktable%share/photoflow%g' src/external/rawspeed/CMakeLists.txt
+#rm -f src/external/rawspeed/CMakeLists.txt.bak
+
 rm -rf /tmp/dt-git
+
