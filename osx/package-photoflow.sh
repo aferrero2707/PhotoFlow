4074bc8d871694df2996f3259a80e5847328c5e0
aferrero2707 <aferrero1975@gmail.com>
2016-11-05 21:10:58 +0100

    Updated travis configuration for OSX

--- osx/package-photoflow.sh
+++ osx/package-photoflow.sh
@@ -54,9 +54,24 @@ function patch () {
 }
 
 # transfer.sh
-function transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
-tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
+function transfer() {
+  if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi 
+  tmpfile=$( mktemp -t transferXXX ); 
+  if tty -s; then 
+    basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); 
+    echo "curl --progress-bar --upload-file \"$1\" \"https://transfer.sh/$basefile\""
+    curl --verbose --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile;   
+  else 
+    echo "curl --progress-bar --upload-file \"-\" \"https://transfer.sh/$1\""
+    curl --verbose --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; 
+  fi; 
+  cat $tmpfile; 
+  rm -f $tmpfile; 
+}
 
+transfer ../icon/photoflow.png
+
+exit
 
 
 cp Info.plist.in Info.plist
