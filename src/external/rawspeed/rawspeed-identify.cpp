4c1b99b2f2e94eb54831c2359de01565b3172bb5
aferrero2707 <aferrero1975@gmail.com>
2016-11-23 16:33:20 +0100

    Updated RAW decoding from Darktable/Rawspeed

--- src/external/rawspeed/rawspeed-identify.cpp
+++ src/external/rawspeed/rawspeed-identify.cpp
@@ -17,18 +17,19 @@
     along with darktable.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "rawspeed/RawSpeed/RawSpeed-API.h"  // IWYU pragma: keep
+#include <cstddef>                           // for size_t
+#include <cstdio>                            // for fprintf, stdout, stderr
+#include <exception>                         // for exception
+#include <memory>                            // for unique_ptr, allocator
+#include <stdint.h>                          // for uint16_t
+#include <string>                            // for string, operator+, basic...
+#include <sys/stat.h>                        // for stat
+#include <vector>                            // for vector
+
 #ifdef _OPENMP
 #include <omp.h>
 #endif
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <memory>
-
-#include "rawspeed/RawSpeed/RawSpeed-API.h"
 
 // define this function, it is only declared in rawspeed:
 int rawspeed_get_number_of_processor_cores()
@@ -42,12 +43,12 @@ int rawspeed_get_number_of_processor_cores()
 
 using namespace RawSpeed;
 
-char *find_cameras_xml(const char *argv0)
+std::string find_cameras_xml(const char *argv0)
 {
   struct stat statbuf;
 
 #ifdef RS_CAMERAS_XML_PATH
-  static char set_camfile[] = RS_CAMERAS_XML_PATH;
+  static const char set_camfile[] = RS_CAMERAS_XML_PATH;
   if (stat(set_camfile, &statbuf))
   {
     fprintf(stderr, "WARNING: Couldn't find cameras.xml in '%s'\n", set_camfile);
@@ -58,34 +59,20 @@ char *find_cameras_xml(const char *argv0)
   }
 #endif
 
+  std::string self(argv0);
+
   // If we haven't been provided with a valid cameras.xml path on compile try relative to argv[0]
-  size_t len = strlen(argv0);
-  char *bindir = (char *) calloc(len+1, sizeof(char));
-  int found_slash = FALSE;
-  for (int i = len-1; i >= 0; i--)
+  std::size_t lastslash = self.find_last_of("/\\");
+  std::string bindir(self.substr(0, lastslash));
+
+  std::string found_camfile(bindir + "/../share/darktable/rawspeed/cameras.xml");
+
+  if (stat(found_camfile.c_str(), &statbuf))
   {
-    if (found_slash)
-    {
-      bindir[i] = argv0[i];
-    }
-    else
-    {
-      bindir[i] = '\0';
-      if (argv0[i] == '/')
-        found_slash = TRUE;
-    }
-  }
-  char *found_camfile = NULL;
-  if (asprintf(&found_camfile, "%s/../share/darktable/rawspeed/cameras.xml", bindir) == -1)
-  {
-    fprintf(stderr, "ERROR: Something is seriously broken here, bailing out");
+    fprintf(stderr, "ERROR: Couldn't find cameras.xml in '%s'\n", found_camfile.c_str());
     return NULL;
   }
-  if (stat(found_camfile, &statbuf))
-  {
-    fprintf(stderr, "ERROR: Couldn't find cameras.xml in '%s'\n", found_camfile);
-    return NULL;
-  }
+
   return found_camfile;
 }
 
@@ -98,8 +85,8 @@ int main(int argc, const char* argv[])
     return 2;
   }
 
-  char *camfile = find_cameras_xml(argv[0]);
-  if (!camfile)
+  const std::string camfile = find_cameras_xml(argv[0]);
+  if (camfile.empty())
   {
     //fprintf(stderr, "ERROR: Couldn't find cameras.xml\n");
     return 2;
@@ -108,7 +95,7 @@ int main(int argc, const char* argv[])
 
   try
   {
-    std::unique_ptr<CameraMetaData> meta(new CameraMetaData(camfile));
+    std::unique_ptr<CameraMetaData> meta(new CameraMetaData(camfile.c_str()));
 
     if(!meta.get())
     {
@@ -119,6 +106,8 @@ int main(int argc, const char* argv[])
 #ifdef __AFL_HAVE_MANUAL_CONTROL
     __AFL_INIT();
 #endif
+
+    fprintf(stderr, "Loading file: \"%s\"\n", argv[1]);
 
     FileReader f((char *) argv[1]);
 
@@ -157,25 +146,25 @@ int main(int argc, const char* argv[])
     fprintf(stdout, "blackLevel: %d\n", r->blackLevel);
     fprintf(stdout, "whitePoint: %d\n", r->whitePoint);
 
-    fprintf(stdout, "blackLevelSeparate: %d %d %d %d\n", 
+    fprintf(stdout, "blackLevelSeparate: %d %d %d %d\n",
                     r->blackLevelSeparate[0], r->blackLevelSeparate[1],
                     r->blackLevelSeparate[2], r->blackLevelSeparate[3]);
 
-    fprintf(stdout, "wbCoeffs: %f %f %f %f\n", 
+    fprintf(stdout, "wbCoeffs: %f %f %f %f\n",
                     r->metadata.wbCoeffs[0], r->metadata.wbCoeffs[1],
                     r->metadata.wbCoeffs[2], r->metadata.wbCoeffs[3]);
 
     fprintf(stdout, "isCFA: %d\n", r->isCFA);
     uint32 filters = r->cfa.getDcrawFilter();
     fprintf(stdout, "filters: %d (0x%x)\n", filters, filters);
-    uint32 bpp = r->getBpp();
+    const uint32 bpp = r->getBpp();
     fprintf(stdout, "bpp: %d\n", bpp);
     uint32 cpp = r->getCpp();
     fprintf(stdout, "cpp: %d\n", cpp);
     fprintf(stdout, "dataType: %d\n", r->getDataType());
 
     // dimensions of uncropped image
-    iPoint2D dimUncropped = r->getUncroppedDim();
+    const iPoint2D dimUncropped = r->getUncroppedDim();
     fprintf(stdout, "dimUncropped: %dx%d\n", dimUncropped.x, dimUncropped.y);
 
     // dimensions of cropped image
@@ -190,14 +179,53 @@ int main(int argc, const char* argv[])
     fprintf(stdout, "pixel_aspect_ratio: %f\n", r->metadata.pixelAspectRatio);
 
     double sum = 0.0f;
-    for(uint32 row = 0; row < ((uint32) dimUncropped.y); row++)
     {
-      uchar8 *data = r->getDataUncropped(0, row);
-      for(uint32 byte = 0; byte < ((uint32) dimUncropped.x*bpp) ; byte++)
-        sum += (double) data[byte];
+      uchar8 *const data = r->getDataUncropped(0, 0);
+
+#ifdef _OPENMP
+#pragma omp parallel for default(none) schedule(static) reduction(+:sum)
+#endif
+      for(size_t k = 0; k < ((size_t) dimUncropped.y*dimUncropped.x*bpp); k++)
+      {
+        sum += (double) data[k];
+      }
+
     }
     fprintf(stdout, "Image byte sum: %lf\n", sum);
-    fprintf(stdout, "Image byte avg: %lf\n", sum/(dimUncropped.y*dimUncropped.x*bpp));
+    fprintf(stdout, "Image byte avg: %lf\n", sum/(double)(dimUncropped.y*dimUncropped.x*bpp));
+
+    if(r->getDataType() == TYPE_FLOAT32)
+    {
+      sum = 0.0f;
+      float *const data = (float *)r->getDataUncropped(0, 0);
+
+#ifdef _OPENMP
+#pragma omp parallel for default(none) schedule(static) reduction(+:sum)
+#endif
+      for(size_t k = 0; k < ((size_t) dimUncropped.y*dimUncropped.x); k++)
+      {
+        sum += (double) data[k];
+      }
+
+      fprintf(stdout, "Image float sum: %lf\n", sum);
+      fprintf(stdout, "Image float avg: %lf\n", sum/(double)(dimUncropped.y*dimUncropped.x));
+    }
+    else if(r->getDataType() == TYPE_USHORT16)
+    {
+      sum = 0.0f;
+      uint16_t *const data = (uint16_t *)r->getDataUncropped(0, 0);
+
+#ifdef _OPENMP
+#pragma omp parallel for default(none) schedule(static) reduction(+:sum)
+#endif
+      for(size_t k = 0; k < ((size_t) dimUncropped.y*dimUncropped.x); k++)
+      {
+        sum += (double) data[k];
+      }
+
+      fprintf(stdout, "Image uint16_t sum: %lf\n", sum);
+      fprintf(stdout, "Image uint16_t avg: %lf\n", sum/(double)(dimUncropped.y*dimUncropped.x));
+    }
   }
   catch(const std::exception &exc)
   {
--- tools/update_darktable.sh
+++ tools/update_darktable.sh
@@ -1,9 +1,43 @@
 #! /bin/bash
 
+wd=$(pwd)
 
-dtdir="$1"
+if [ -e $HOME/Source/dt-git/darktable ]; then
+    cd $HOME/Source/dt-git/darktable
+    git pull origin master
+else
+    mkdir -p $HOME/Source/dt-git
+    cd $HOME/Source/dt-git
+    git clone https://github.com/darktable-org/darktable.git
+fi
+dtdir=$HOME/Source/dt-git/darktable
+cd "$wd"
+
+rm -rf src/external/rawspeed
+
+echo "cp $dtdir/cmake/modules/CheckCompilerFlagAndEnableIt.cmake cmake/modules"
+cp "$dtdir/cmake/modules/CheckCompilerFlagAndEnableIt.cmake" cmake/modules
+
+echo "cp $dtdir/cmake/modules/CheckCCompilerFlagAndEnableIt.cmake cmake/modules"
+cp "$dtdir/cmake/modules/CheckCCompilerFlagAndEnableIt.cmake" cmake/modules 
+
+echo "cp $dtdir/cmake/modules/CheckCXXCompilerFlagAndEnableIt.cmake cmake/modules"
+cp "$dtdir/cmake/modules/CheckCXXCompilerFlagAndEnableIt.cmake" cmake/modules 
+
+cp -a "$dtdir/src/external/rawspeed" src/external
 cp "$dtdir/src/external/adobe_coeff.c" "$dtdir/src/external/wb_presets.c" src/dt/external
 cp "$dtdir/src/common/colormatrices.c" src/dt/common
 cp "$dtdir/src/common/srgb_tone_curve_values.h" src/dt/common
 #"$dtdir/src/common/colorspaces.h"
 #"$dtdir/src/common/colorspaces.c"
+
+# Patches
+sed -i .bak 's/TRUE/(boolean)TRUE/g' src/external/rawspeed/RawSpeed/DngDecoderSlices.cpp
+sed -i .bak 's/return true/return (boolean)TRUE/g' src/external/rawspeed/RawSpeed/X3fDecoder.cpp
+sed -i .bak 's/return false/return (boolean)FALSE/g' src/external/rawspeed/RawSpeed/X3fDecoder.cpp
+#rm -f src/external/rawspeed/RawSpeed/DngDecoderSlices.cpp.bak
+sed -i .bak 's%darktable%photoflow%g' src/external/rawspeed/CMakeLists.txt
+#rm -f src/external/rawspeed/CMakeLists.txt.bak
+
+rm -rf /tmp/dt-git
+
