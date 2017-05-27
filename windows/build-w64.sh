b9d42a7b223f1aa241f37859c4bba4f4e82c4a77
aferrero2707 <aferrero1975@gmail.com>
2017-05-27 18:59:06 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -1,11 +1,8 @@
-crossroad install libfftw3-3 fftw3-devel
+crossroad install libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libfftw3-3 fftw3-devel libexif-devel
 ls ~/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig
 cat ~/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig/fftw3.pc
-exit
-
-crossroad install libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libfftw3-3 fftw3-devel libexif-devel
-wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
-tar xzvf vips-8.4.5.tar.gz
-(cd vips-8.4.5 && crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make && make install)
+#wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
+#tar xzvf vips-8.4.5.tar.gz
+#(cd vips-8.4.5 && crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make && make install)
 
 crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
