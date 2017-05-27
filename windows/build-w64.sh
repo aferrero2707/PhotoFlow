99bde7e3c6a0f73c7026e367ca865677d07abe8c
aferrero2707 <aferrero1975@gmail.com>
2017-05-27 21:22:23 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -1,13 +1,10 @@
 which crossroad
 cat /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
-exit
+sudo echo "" >> /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
+sudo echo "SET(PKG_CONFIG_EXECUTABLE x86_64-w64-mingw32-pkg-config)" >> /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
 
 crossroad install pkg-config libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libfftw3-3 fftw3-devel libexif-devel 
-ls ~/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig
-cat ~/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig/fftw3.pc
 #wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
 #tar xzvf vips-8.4.5.tar.gz
 #(cd vips-8.4.5 && crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make && make install)
-which pkg-config
-export PKG_CONFIG_PATH=$HOME/.local/share/crossroad/roads/w64/phf-build/lib:$PKG_CONFIG_PATH
 crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
