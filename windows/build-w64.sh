078bd95dead96325fe2956fcccfa7149d4cf6c6d
aferrero2707 <aferrero1975@gmail.com>
2017-05-27 19:17:34 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -4,5 +4,6 @@ cat ~/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig/fftw3.pc
 #wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
 #tar xzvf vips-8.4.5.tar.gz
 #(cd vips-8.4.5 && crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make && make install)
-
+which pkg-config
+export PKG_CONFIG_PATH=$HOME/.local/share/crossroad/roads/w64/phf-build/lib:$PKG_CONFIG_PATH
 crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
