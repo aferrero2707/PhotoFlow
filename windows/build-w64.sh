7e0ab648986c2c8ea211dea9a2fee923ada8b242
aferrero2707 <aferrero1975@gmail.com>
2017-05-27 21:29:01 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -1,7 +1,8 @@
 which crossroad
-cat /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
 sudo echo "" >> /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
 sudo echo "SET(PKG_CONFIG_EXECUTABLE x86_64-w64-mingw32-pkg-config)" >> /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
+cat /usr/local/share/crossroad/scripts/cmake/toolchain-w64.cmake
+exit
 
 crossroad install pkg-config libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libfftw3-3 fftw3-devel libexif-devel 
 #wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
