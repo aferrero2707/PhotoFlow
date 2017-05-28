05cba6cbfae31fc9325f9921e688548c241aac4b
aferrero2707 <aferrero1975@gmail.com>
2017-05-28 14:51:47 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -1,5 +1,6 @@
 #$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
-#exit
+ls /usr/lib/gcc
+exit
 
 $HOME/inst/bin/crossroad install pkg-config libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libxml2-tools libfftw3-3 fftw3-devel libexif-devel 
 
