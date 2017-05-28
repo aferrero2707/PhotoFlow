b256d4e3d7fee852d50981ae9d23cfae4b6aa09e
aferrero2707 <aferrero1975@gmail.com>
2017-05-28 18:21:28 +0200

    [Travis CI] testing win64 x-build

--- windows/build-w64.sh
+++ windows/build-w64.sh
@@ -10,7 +10,7 @@ $HOME/inst/bin/crossroad install pkg-config libtiff-devel libpng-devel libjpeg8-
 # VIPS
 wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
 tar xzvf vips-8.4.5.tar.gz
-(cd vips-8.4.5 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make && make install)
+(cd vips-8.4.5 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install)
 
 #PhotoFlow
-$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
+$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make -j 3 && make install
