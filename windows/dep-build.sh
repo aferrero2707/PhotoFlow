a6a9c2e66c6b22ea6a5f809ac3412e4ce8a39835
aferrero2707 <aferrero1975@gmail.com>
2017-05-28 21:47:28 +0200

    [Travis CI] testing win64 x-build

--- windows/dep-build.sh
+++ windows/dep-build.sh
@@ -1,7 +1,19 @@
 # PugiXML
-(git clone https://github.com/zeux/pugixml.git && cd pugixml && $HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON && make && make install)
+git clone https://github.com/zeux/pugixml.git && cd pugixml 
+if [ $? -ne 0 ]; then exit 1; fi
+$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON && make && make install
+if [ $? -ne 0 ]; then exit 1; fi
+
+cd ..
 
 # VIPS
 wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
+if [ $? -ne 0 ]; then exit 1; fi
+
 tar xzvf vips-8.4.5.tar.gz
-(cd vips-8.4.5 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install)
+if [ $? -ne 0 ]; then exit 1; fi
+
+cd vips-8.4.5 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install
+if [ $? -ne 0 ]; then exit 1; fi
+
+exit 0
--- windows/dep-install.sh
+++ windows/dep-install.sh
@@ -1,5 +1,11 @@
+#! /bin/bash
 #$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=Release -DBUNDLED_LENSFUN=ON .. && make && make install
 ls /usr/lib/gcc
 #exit
 
 $HOME/inst/bin/crossroad install pkg-config libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libxml2-tools libfftw3-3 fftw3-devel libexif-devel 
+
+if [ $? -ne 0 ]; then 
+	exit 1
+fi
+exit 0
