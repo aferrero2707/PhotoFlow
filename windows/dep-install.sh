ae2549efb38a6003f38514cb7c70424016f0cabb
aferrero2707 <aferrero1975@gmail.com>
2017-05-31 22:53:36 +0200

    [Travis CI] testing win64 x-build

--- windows/dep-install.sh
+++ windows/dep-install.sh
@@ -5,7 +5,7 @@ ls /usr/lib/gcc
 
 for counter in $(seq 1 10); do
 echo "try $counter"
-$HOME/inst/bin/crossroad install libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libxml2-tools libfftw3-3 fftw3-devel libexif-devel 
+$HOME/inst/bin/crossroad install libtiff5 libtiff-devel libpng16-16 libpng-devel libjpeg8 libjpeg8-devel libgtkmm-2_4-1 gtkmm2-devel liborc-0_4-0 liborc-devel libexiv2 libexiv2-devel liblcms2-2 liblcms2-devel libxml2-2 libxml2-devel libxml2-tools libfftw3-3 fftw3-devel libexif12 libexif-devel  
 #$HOME/inst/bin/crossroad install libxml2-devel libxml2-tools
 if [ $? -eq 0 -o $counter -eq 10 ]; then 
 	break
