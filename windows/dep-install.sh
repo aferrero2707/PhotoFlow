12ba9a2a45053006f8e899f61129d8fb70b08e82
aferrero2707 <aferrero1975@gmail.com>
2017-05-31 14:25:18 +0200

    [Travis CI] testing win64 x-build

--- windows/dep-install.sh
+++ windows/dep-install.sh
@@ -3,8 +3,13 @@
 ls /usr/lib/gcc
 #exit
 
+for counter in $(seq 1 10); do
 $HOME/inst/bin/crossroad install libtiff-devel libpng-devel libjpeg8-devel gtkmm2-devel liborc-devel libexiv2-devel liblcms2-devel libxml2-devel libxml2-tools libfftw3-3 fftw3-devel libexif-devel 
 #$HOME/inst/bin/crossroad install libxml2-devel libxml2-tools
+if [ $? -eq 0 -o $counter -eq 10 ]; then 
+	break
+fi
+done
 
 ls $HOME/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig
 cat $HOME/.local/share/crossroad/roads/w64/phf-build/lib/pkgconfig/libxml-2.0.pc
