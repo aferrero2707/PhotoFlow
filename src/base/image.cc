f5c55a7dd818f5fc749f2ca3aece9e06e222a750
aferrero2707 <aferrero1975@gmail.com>
2017-06-13 22:39:48 +0200

    [base] Improved code for embedding of ICC profiles in exported images.        Now it is compatible with Exiv2 versions older than 0.26

--- src/base/image.cc
+++ src/base/image.cc
@@ -1122,8 +1122,19 @@ void PF::Image::do_export_merged( std::string filename )
             if( !vips_image_get_blob( outimg, VIPS_META_ICC_NAME,
                                      &iccdata, &iccdata_length ) ) {
               Exiv2::byte *iccdata2 = (Exiv2::byte *)iccdata;
-              Exiv2::DataBuf iccbuf(iccdata2, iccdata_length);
-              exiv2_image->setIccProfile( iccbuf, true );
+              //Exiv2::DataBuf iccbuf(iccdata2, iccdata_length);
+              //exiv2_image->setIccProfile( iccbuf, true );
+
+              Exiv2::ExifKey            key("Exif.Image.InterColorProfile");
+              Exiv2::ExifData::iterator pos   = exiv2_image->exifData().findKey(key);
+              bool                      found = pos != exiv2_image->exifData().end();
+              if ( iccdata ) {
+                  Exiv2::DataValue value(iccdata2,iccdata_length);
+                  if ( found ) pos->setValue(&value);
+                  else     exiv2_image->exifData().add(key,&value);
+              } else {
+                  if ( found ) exiv2_image->exifData().erase(pos);
+              }
             }
             exiv2_image->writeMetadata();
           }
