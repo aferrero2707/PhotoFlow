518966a5102f07720b0e4d6cc3d7aefea214bea3
aferrero2707 <aferrero1975@gmail.com>
2017-06-19 22:27:47 +0200

    [travis ci] upgraded VIPS version to 8.5.6

--- windows/dep-build.sh
+++ windows/dep-build.sh
@@ -7,13 +7,13 @@ if [ $? -ne 0 ]; then exit 1; fi
 cd ..
 
 # VIPS
-wget http://www.vips.ecs.soton.ac.uk/supported/8.4/vips-8.4.5.tar.gz
+wget https://github.com/jcupitt/libvips/releases/download/v8.5.6/vips-8.5.6.tar.gz
 if [ $? -ne 0 ]; then exit 1; fi
 
-tar xzvf vips-8.4.5.tar.gz
+tar xzvf vips-8.5.6.tar.gz
 if [ $? -ne 0 ]; then exit 1; fi
 
-cd vips-8.4.5 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install
+cd vips-8.5.6 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install
 if [ $? -ne 0 ]; then exit 1; fi
 
 exit 0
