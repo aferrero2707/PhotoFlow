433aa6182023c3457ee1a99fd8384856f4246ab0
aferrero2707 <aferrero1975@gmail.com>
2017-07-24 22:13:30 +0200

    [travis ci] removed use of --run crossroad argument

--- windows/dep-build.sh
+++ windows/dep-build.sh
@@ -1,3 +1,8 @@
+#! /bin/bash
+arch=$1
+
+echo "Compiling dependencies"
+$HOME/inst/bin/crossroad $arch phf-build <<EOF
 # PugiXML
 git clone https://github.com/zeux/pugixml.git && cd pugixml 
 if [ $? -ne 0 ]; then exit 1; fi
@@ -13,7 +18,8 @@ if [ $? -ne 0 ]; then exit 1; fi
 tar xzvf vips-8.5.6.tar.gz
 if [ $? -ne 0 ]; then exit 1; fi
 
-cd vips-8.5.6 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp && make -j 3 && make install
+cd vips-8.5.6 && $HOME/inst/bin/crossroad configure --disable-gtk-doc --disable-gtk-doc-html --disable-introspection --enable-debug=no --without-python --without-magick --without-libwebp  --without-orc && make -j 3 && make install
 if [ $? -ne 0 ]; then exit 1; fi
 
 exit 0
+EOF
--- windows/phf-build.sh
+++ windows/phf-build.sh
@@ -1,4 +1,10 @@
+#! /bin/bash
+arch=$1
+
+echo "Compiling photoflow"
+$HOME/inst/bin/crossroad $arch phf-build <<EOF
 #PhotoFlow
 $HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUNDLED_LENSFUN=ON .. && make -j 3 && make install
 if [ $? -ne 0 ]; then exit 1; fi
 exit 0
+EOF
