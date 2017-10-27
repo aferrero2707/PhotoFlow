b737f0f13dd57361ef4610871b12b07da2e1a94d
aferrero2707 <aferrero1975@gmail.com>
2017-10-27 22:14:37 +0200

    [travis ci] updated windows 64 scripts

--- windows/phf-build.sh
+++ windows/phf-build.sh
@@ -2,9 +2,8 @@
 arch=$1
 
 echo "Compiling photoflow"
-$HOME/inst/bin/crossroad $arch phf-build <<EOF
-#PhotoFlow
-$HOME/inst/bin/crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUNDLED_LENSFUN=ON .. && make -j 3 && make install
+#$HOME/inst/bin/crossroad $arch phf-build <<EOF
+crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUNDLED_LENSFUN=ON .. && make -j 3 && make install
 if [ $? -ne 0 ]; then exit 1; fi
-exit 0
-EOF
+#exit 0
+#EOF
