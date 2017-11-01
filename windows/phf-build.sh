4dcfbba1110754eda97ac64aa17effb19be9b031
aferrero2707 <aferrero1975@gmail.com>
2017-11-01 17:59:47 +0100

    Update phf-build.sh

--- windows/phf-build.sh
+++ windows/phf-build.sh
@@ -11,5 +11,7 @@ echo "Compiling photoflow"
 #$HOME/inst/bin/crossroad $arch phf-build <<EOF
 crossroad cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUNDLED_LENSFUN=ON /sources && make -j 2 && make install
 if [ $? -ne 0 ]; then exit 1; fi
+cd /sources
+./windows/package-w64.sh
 #exit 0
 #EOF
