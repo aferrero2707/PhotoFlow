a77905ab1c421fd528d80887f21fa7cce0798451
aferrero2707 <aferrero1975@gmail.com>
2018-11-30 08:39:06 +0100

    Update mainwindow.cc

--- src/gui/mainwindow.cc
+++ src/gui/mainwindow.cc
@@ -966,11 +966,13 @@ void PF::MainWindow::on_button_open_clicked()
       std::cout<<"MainWindow::on_button_open_clicked: "<<e.what()<<std::endl;
     }
     std::cout<<"MainWindow::on_button_open_clicked: filename=\""<<filename<<"\""<<std::endl;
+    /*
     Glib::RefPtr<Gio::File> ftest = Gio::File::create_for_path(filename);
     if( !ftest->query_exists() ) {
       std::cout<<"MainWindow::on_button_open_clicked: file \""<<filename<<"\" not found"<<std::endl;
       return;
     }
+    */
     open_image( filename );
     /*
     char* fullpath = realpath( filename.c_str(), NULL );
