f2215732ee53be4a82b7d756362ad9bb5b5c7a32
aferrero2707 <aferrero1975@gmail.com>
2019-04-30 09:46:04 +0200

    Re-enabled old version of tone mapping tool in the UI

--- src/gui/operationstree.cc
+++ src/gui/operationstree.cc
@@ -297,11 +297,11 @@ PF::OperationsTreeDialog::OperationsTreeDialog( Image* img, LayerWidget* lw ):
   op_color.get_tree().add_op( _("Color Adjustments"), "color_correction" );
   op_color.get_tree().add_op( _("Desaturate"), "desaturate" );
   op_color.get_tree().add_op( _("Curves"), "curves" );
-  op_color.get_tree().add_op( _("Shadows/Highlights old"), "shadows_highlights" );
   op_color.get_tree().add_op( _("Shadows/Highlights"), "shadows_highlights_v2" );
-  op_color.get_tree().add_op( _("Dynamic range compressor"), "dynamic_range_compressor_v2" );
-  //op_color.get_tree().add_op( _("Tone mapping old"), "tone_mapping" );
   op_color.get_tree().add_op( _("Tone mapping"), "tone_mapping_v2" );
+  op_color.get_tree().add_op( _("Dynamic range compressor"), "dynamic_range_compressor_v2" );
+  op_color.get_tree().add_op( _("Shadows/Highlights old"), "shadows_highlights" );
+  op_color.get_tree().add_op( _("Tone mapping old"), "tone_mapping" );
   op_color.get_tree().add_op( _("Channel Mixer"), "channel_mixer" );
   //op_color.get_tree().add_op( "Brightness/Contrast"), "brightness_contrast" );
   op_color.get_tree().add_op( _("Noise"), "noise_generator" );
