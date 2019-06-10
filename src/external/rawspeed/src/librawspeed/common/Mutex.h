0c79ee254eab9e4a44c7aeca9e43dc7990f14dd8
aferrero2707 <aferrero1975@gmail.com>
2019-06-10 19:06:35 +0200

    Fixed fatal compilation warning

--- src/external/rawspeed/src/librawspeed/common/Mutex.h
+++ src/external/rawspeed/src/librawspeed/common/Mutex.h
@@ -77,12 +77,12 @@ public:
   // Acquire/lock this mutex exclusively.  Only one thread can have exclusive
   // access at any one time.  Write operations to guarded data require an
   // exclusive lock.
-  void __attribute__((const)) Lock()  ACQUIRE() {
+  void /*__attribute__((const))*/ Lock() /*const*/ ACQUIRE() {
     // NOP, since there is no mutex. only here to still check for proper locking
   }
 
   // Release/unlock an exclusive mutex.
-  void __attribute__((const)) Unlock() RELEASE() {
+  void /*__attribute__((const))*/ Unlock() /*const*/ RELEASE() {
     // NOP, since there is no mutex. only here to still check for proper locking
   }
 
